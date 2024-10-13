016a850964d5a13a26011a56d2a22bea13c7b117
Siddharth <sidtdm101@gmail.com>
2024-10-13 14:36:47 +0530

    Small change

--- core/src/main/java/com/game/angrybirds/Homescreen.java
+++ core/src/main/java/com/game/angrybirds/Homescreen.java
@@ -29,7 +29,7 @@ public class Homescreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.1f,0.1f,0.3f,1);
+        Gdx.gl.glClearColor(1f,1f,1f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         float xOffset = (1560-1280)/2f;
 
