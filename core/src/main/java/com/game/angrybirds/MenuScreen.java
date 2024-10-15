3407b8ed6b74168886069408535210eb57561c00
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-10-15 11:24:40 +0530

    MenuScreen.java

--- core/src/main/java/com/game/angrybirds/MenuScreen.java
+++ core/src/main/java/com/game/angrybirds/MenuScreen.java
@@ -36,7 +36,7 @@ public class MenuScreen extends InputAdapter implements Screen  {
 
     @Override
     public void render(float delta) {
-        // Clear the screen
+
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.3f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
