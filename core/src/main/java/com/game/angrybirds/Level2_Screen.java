8fb9b7801f56868bf338b94bf8f0dc226fa0456e
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-10-24 23:08:04 +0530

    Update Level2_Screen.java

--- core/src/main/java/com/game/angrybirds/Level2_Screen.java
+++ core/src/main/java/com/game/angrybirds/Level2_Screen.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.World;
+import com.game.angrybirds.bird.red_bird;
 
 public class Level2_Screen extends InputAdapter implements Screen {
     private final Main game;
