0208f626e387901cccf50848c9ab604dfd6b5d65
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-10-24 22:30:36 +0530

    Update Homescreen.java

--- core/src/main/java/com/game/angrybirds/Homescreen.java
+++ core/src/main/java/com/game/angrybirds/Homescreen.java
@@ -1,11 +1,13 @@
-package com.game.angrybirds;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.World;
+import com.game.angrybirds.bird.red_bird;
 
 public class Homescreen extends InputAdapter implements Screen  {
     private final Main game;
