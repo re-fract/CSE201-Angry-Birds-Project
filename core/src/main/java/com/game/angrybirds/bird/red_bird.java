59c4612ac84e7cfe6791d99b893ffc0911c4509c
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-10-25 10:39:01 +0530

    Update and rename Red_Bird.java to red_bird.java

--- core/src/main/java/com/game/angrybirds/bird/red_bird.java
+++ core/src/main/java/com/game/angrybirds/bird/red_bird.java
@@ -2,34 +2,26 @@ package com.game.angrybirds.bird;
 
 import com.badlogic.gdx.physics.box2d.*;
 
-public class Red_Bird {
+public class red_bird extends parent_bird{
     private Body body;
-    private int health;
-    public Red_Bird(World world, float x, float y) {
+    CircleShape shape;
+    public red_bird(World world, float x, float y) {
+        super(100,50);
+
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set(x, y); //initial position
-        health=20;
-
+        bodyDef.position.set(x, y);
         body = world.createBody(bodyDef);
 
-        CircleShape shape = new CircleShape();
+        shape = new CircleShape();
         shape.setRadius(30f);
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
         fixtureDef.density = 1f;
         fixtureDef.friction = 3f;
-        fixtureDef.restitution = 6f;  // Bounce
-
+        fixtureDef.restitution = 6f;
         body.createFixture(fixtureDef);
-        shape.dispose();
     }
-
-    public Body getBody() {
-        return body;
-    }
-
-
 }
 
