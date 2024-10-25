5686d89ddcda6191abddcae1446123fb9f79c045
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-10-25 12:40:01 +0530

    Update ParentBird.java

--- core/src/main/java/com/game/angrybirds/bird/ParentBird.java
+++ core/src/main/java/com/game/angrybirds/bird/ParentBird.java
@@ -1,7 +1,7 @@
 package com.game.angrybirds.bird;
 public abstract class ParentBird {
-    private int Health_point_Dec;
-    private int speed;
+    public int Health_point_Dec;
+    public int speed;
     public ParentBird(int Health_point_Dec, int speed) {
         this.Health_point_Dec = Health_point_Dec;
         this.speed = speed;
