936b2e76fe9c31dddf94968a58bd588f8e6122b7
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-10-25 10:44:41 +0530

    Create Yellow_Bird.java

---
+++ core/src/main/java/com/game/angrybirds/bird/Yellow_Bird.java
@@ -0,0 +1,29 @@
+package com.game.angrybirds.bird;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class Yellow_Bird extends parent_bird{
+    private Body body;
+    private PolygonShape shape;
+    public Yellow_Bird(World world, float x, float y) {
+        super( 200,100);
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(x, y);
+        body = world.createBody(bodyDef);
+
+        shape = new PolygonShape();
+        Vector2[] vertices = new Vector2[3];
+        vertices[0] = new Vector2(0, 0);
+        vertices[1] = new Vector2(1, 0);
+        vertices[2] = new Vector2(0.5f, 1);
+        shape.set(vertices);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+        fixtureDef.friction = 3f;
+        fixtureDef.restitution = 6f;
+        body.createFixture(fixtureDef);
+    }
+}
