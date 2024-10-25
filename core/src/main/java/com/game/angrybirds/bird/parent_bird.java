51854dd16eff0d8c7acd33c193c31790589842e2
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-10-25 10:37:30 +0530

    Update and rename Parent_Bird.java to parent_bird.java

---
+++ core/src/main/java/com/game/angrybirds/bird/parent_bird.java
@@ -0,0 +1,9 @@
+package com.game.angrybirds.bird;
+public abstract class parent_bird {
+    private int Health_point_Dec;
+    private int speed;
+    public parent_bird(int Health_point_Dec, int speed) {
+        this.Health_point_Dec = Health_point_Dec;
+        this.speed = speed;
+    }
+}
