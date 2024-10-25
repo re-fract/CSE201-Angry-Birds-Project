b5593e40fec2debf84897bd249ebe90f5f2804dd
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-10-25 12:49:37 +0530

    Update ParentBird.java

--- core/src/main/java/com/game/angrybirds/bird/ParentBird.java
+++ core/src/main/java/com/game/angrybirds/bird/ParentBird.java
@@ -7,9 +7,14 @@ public abstract class ParentBird {
     public float m;
     public int width;
     public int height;
-    public ParentBird(int Health_point_Dec, int speed) {
+    public parent_bird(int Health_point_Dec, int speed,Texture texture,float l,float m,int width,int height) {
         this.Health_point_Dec = Health_point_Dec;
         this.speed = speed;
+        this.texture = texture;
+        this.l = l;
+        this.m = m;
+        this.width = width;
+        this.height = height;
     }
     public void render(SpriteBatch batch) {
         Vector2 position = new Vector2(l,m);
