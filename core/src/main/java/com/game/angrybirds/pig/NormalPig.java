11859296fbd1ee6663c31376d1e844f289f63d80
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-11-26 23:44:45 +0530

    Update NormalPig.java with flag=2

--- core/src/main/java/com/game/angrybirds/pig/NormalPig.java
+++ core/src/main/java/com/game/angrybirds/pig/NormalPig.java
@@ -3,8 +3,12 @@ package com.game.angrybirds.pig;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class NormalPig extends ParentPig{
+import java.io.Serializable;
+
+public class NormalPig extends ParentPig implements Serializable {
+
     public NormalPig(World world,int x, int y, int health, float radius) {
-        super(world,"pig.png", x, y, health, radius);
+        super(world,"pig.png", x, y, health, radius,3);
+
     }
 }
