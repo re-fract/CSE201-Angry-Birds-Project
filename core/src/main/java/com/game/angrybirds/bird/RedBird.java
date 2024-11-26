21072a1fe1b880424fabccea88a01ecc3ed89d90
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-11-26 23:40:23 +0530

    Update RedBird.java with flag=1

--- core/src/main/java/com/game/angrybirds/bird/RedBird.java
+++ core/src/main/java/com/game/angrybirds/bird/RedBird.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-public class RedBird extends ParentBird{
+
+import java.io.Serializable;
+
+public class RedBird extends ParentBird implements Serializable {
     public RedBird(World world, float x, float y) {
-        super(world,100,"redbird.png",x,y);
+        super(world,100,"redbird.png",x,y,1);
     }
 }
