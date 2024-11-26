6e1e4e3d4ea947505459dae2dfffcf983e737895
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-11-26 23:52:17 +0530

    Update WoodBlock.java with flag=2

--- core/src/main/java/com/game/angrybirds/block/WoodBlock.java
+++ core/src/main/java/com/game/angrybirds/block/WoodBlock.java
@@ -2,8 +2,10 @@ package com.game.angrybirds.block;
 
 import com.badlogic.gdx.physics.box2d.World;
 
-public class WoodBlock extends ParentBlock{
-    public WoodBlock(World world,int x, int y) {
-        super(world, "woodblock.png", x, y, 3, 100, 50);
+import java.io.Serializable;
+
+public class WoodBlock extends ParentBlock implements Serializable {
+    public WoodBlock(World world,int x, int y,int health) {
+        super(world, "woodblock.png", x, y, health, 100, 50,2);
     }
 }
