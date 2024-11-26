bf04f8bd91733889257e30fbf69f9025ab5336bf
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-11-26 23:47:03 +0530

    Update PrincessPig.java with flag=2

--- core/src/main/java/com/game/angrybirds/pig/PrincessPig.java
+++ core/src/main/java/com/game/angrybirds/pig/PrincessPig.java
@@ -3,9 +3,13 @@ package com.game.angrybirds.pig;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class PrincessPig extends ParentPig{
+import java.io.Serializable;
+
+public class PrincessPig extends ParentPig implements Serializable {
 
     public PrincessPig(World world, int x, int y, int health, float radius) {
-        super(world,"pig_princess.png", x, y, health, radius);
+        super(world,"pig_princess.png", x, y, health, radius,2);
+
     }
 }
+
