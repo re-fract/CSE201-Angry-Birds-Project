9ad17da7bdbdb2124eb5f0984717f6f023cf8783
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-11-26 23:51:08 +0530

    Update GlassBlock.java with flag=1

--- core/src/main/java/com/game/angrybirds/block/GlassBlock.java
+++ core/src/main/java/com/game/angrybirds/block/GlassBlock.java
@@ -2,8 +2,10 @@ package com.game.angrybirds.block;
 
 import com.badlogic.gdx.physics.box2d.World;
 
-public class GlassBlock extends ParentBlock {
-    public GlassBlock(World world, int x, int y) {
-        super(world, "glassblock.png", x, y, 1, 100, 50);
+import java.io.Serializable;
+
+public class GlassBlock extends ParentBlock implements Serializable {
+    public GlassBlock(World world, int x, int y,int health) {
+        super(world, "glassblock.png", x, y, health, 100, 50,1);
     }
 }
