24e4097535a501345701e272602d5a6120c675dc
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-11-26 23:43:55 +0530

    Update CrownPig.java with flag=1

--- core/src/main/java/com/game/angrybirds/pig/CrownPig.java
+++ core/src/main/java/com/game/angrybirds/pig/CrownPig.java
@@ -3,9 +3,13 @@ package com.game.angrybirds.pig;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class CrownPig extends ParentPig{
+import java.io.Serializable;
+
+public class CrownPig extends ParentPig implements Serializable {
+
 
     public CrownPig(World world, int x, int y, int health, float radius) {
-        super(world,"pig_crown.png", x, y, health, radius);
+        super(world,"pig_crown.png", x, y, health, radius,1);
+
     }
 }
