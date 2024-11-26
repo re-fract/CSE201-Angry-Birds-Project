5574b63aaba437a2830fbbe806f3bad5bef535db
ritvikshekhar <155644773+ritvikshekhar@users.noreply.github.com>
2024-11-26 23:37:44 +0530

    Update BlackBird.java with flag having 3

--- core/src/main/java/com/game/angrybirds/bird/BlackBird.java
+++ core/src/main/java/com/game/angrybirds/bird/BlackBird.java
@@ -3,10 +3,12 @@ package com.game.angrybirds.bird;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.*;
 
-public class BlackBird extends ParentBird{
+import java.io.Serializable;
+
+public class BlackBird extends ParentBird implements Serializable {
     private Body body;
     private CircleShape shape;
     public BlackBird(World world, float x, float y) {
-        super(world,300,"blackbird.png",x,y);
+        super(world,300,"blackbird.png",x,y,3);
     }
 }
